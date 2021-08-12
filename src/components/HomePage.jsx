2b9e076e64a6f49f24cfae98ffa81da533ebddfb
Harsh Tiwari <harsh.tiwari1995@gmail.com>
2021-08-12 17:17:25 +0530

    addded bootstrap

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
- import "./homepage.css";
+import "./homepage.css";
 import axios from "axios";
- const HomePage = () => {
+const HomePage = () => {
     const [char, setChar] = useState([]);
     const [search, setSearch] = useState("");
     const [data, setData] = useState([]);
-    const [offset, setOffset]= useState(0)
-    const[buttonDis, setButtonDis] = useState(true)
-    const[nbuttonDis, setNButtonDis] = useState(false)
+    const [offset, setOffset] = useState(0)
+    const [buttonDis, setButtonDis] = useState(true)
+    const [nbuttonDis, setNButtonDis] = useState(false)
     useEffect(() => {
         axios
             .get(`https://www.breakingbadapi.com/api/characters?limit=4&offset=${offset}`)
@@ -32,38 +32,35 @@ import axios from "axios";
                 setData(dataArray);
             });
     };
-    const nexthandle = () =>{
-        setOffset(offset+4)
+    const nexthandle = () => {
+        setOffset(offset + 4)
         setButtonDis(false)
-        if(offset===56)
-        setNButtonDis(true)
-        
-    
+        if (offset === 56)
+            setNButtonDis(true)
+
+
     }
-    const prevHandle = () =>{
-        setOffset(offset-4)
-        if(offset===4)
-        setButtonDis(true)
-        
+    const prevHandle = () => {
+        setOffset(offset - 4)
+        if (offset === 4)
+            setButtonDis(true)
+
     }
     return (
         <>
-              <div id="main"> 
+            <div className="input-group md-3 justify-content-center">
+                <label>
+                    <input type="text" className="form-control" placeholder="Search" aria-label="Search" value={search} onChange={searchField} />
+                </label>
+            </div>
 
-                  <input 
-                     className="center"
-                    type="text"
-                    value={search}
-                    placeholder="Search Characters"
-                    onChange={searchField}
-                />
-            </div> 
             {search === "" ? (
                 <div className="container">
                     <div className="button">
+                        <button onClick={() => prevHandle()} disabled={buttonDis} type="button" className="btn btn-primary">Previous</button>
+                        <button onClick={() => nexthandle()} disabled={nbuttonDis} type="button" className="btn btn-primary">Next</button>
 
-                    <button onClick={()=>prevHandle()} disabled={buttonDis}>{"<"}</button>
-                    <button onClick={()=>nexthandle()} disabled={nbuttonDis}>{">"}</button>
+                        
                     </div>
 
                     <div className="imageContainer">
@@ -75,8 +72,8 @@ import axios from "axios";
                                 rel="noreferrer"
                             >
 
-                                <figure>
-                                    <img src={el.img} alt={el.name} />
+                                <figure className="card">
+                                    <img src={el.img} className="card-img-top" alt={el.name} />
                                     <figcaption>
                                         <p><strong>Name</strong>: {el.name}</p>
                                         <p><strong>DOB</strong>: {el.birthday}</p>
@@ -121,5 +118,5 @@ import axios from "axios";
             )}
         </>
     );
- };
- export default HomePage;
+};
+export default HomePage;
